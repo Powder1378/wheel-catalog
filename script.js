0c3ae44c3032b2a8fffe557c9e88f6d8c77d6237
powderrrrrr <116692891+Powder1378@users.noreply.github.com>
2025-06-12 09:41:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sheetID = "1CNIWfNKE-of46T1-B8YalUG_Qe2vJOKdYO8J62MhyW4"; // 例: 1CNIWfNKE-of46T1-B8YalUG_Qe2vJOKdYO8J62MhyW4
-const sheetName = "シート1"; // タブ名（例: Sheet1）
+const sheetName = "wheeldata"; // タブ名（例: Sheet1）
 const url = `https://opensheet.elk.sh/${sheetID}/${sheetName}`;
 
 const container = document.getElementById("wheel-container");
