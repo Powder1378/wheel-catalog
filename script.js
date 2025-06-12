66f10043138db53aac60be866db8bd2c7c1f4f1c
powderrrrrr <116692891+Powder1378@users.noreply.github.com>
2025-06-12 11:21:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-const sheetID = "1CNIWfNKE-of46T1-B8YalUG_Qe2vJOKdYO8J62MhyW4";
-const sheetName = "wheeldata";
-const url = `https://opensheet.elk.sh/${sheetID}/${sheetName}`;
+const baseHtml = document.querySelector('.spreadsheets--item.js-base');
+const spreadsheets = document.querySelector('.spreadsheets');
+const apiURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjAVelBMOc22-gML36B6PALAP4tiKN1iy7VsvcxJnG2JC15sNTGAehPOwwI9l5GgS6xWbAWUWsvvpY1DjrjABpMjSqImbXNny9OJCK8pUKTn0zFNhxailcrUU0VCNRpHKS8yQ2EmMESAwNFDc8G2dhAeLwVqBERMSgmJYJxbZT4K7SMW6LLHwbbtUqp3fYQt7m0zwZGBX5j-2D0X9Ad9BtpdpLY0_y8pHNvey1DW4_qMJ2-ck7dCiCbxi2MCNpuPkq0rESSCAdfLW8zJqzZvRIOLP0_vg&lib=MEX3zXkwI-AmcLcHj6JU7WTzbqBIoUkXV';
 
-const container = document.getElementById("wheel-container");
-
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(item => {
-      const card = document.createElement("div");
-      card.className = "wheel-card";
-
-      card.innerHTML = `
-        <img src="${item.image}" alt="${item.name}" />
-        <h3>${item.name}</h3>
-      `;
-
-      container.appendChild(card);
-    });
-  })
-  .catch(err => {
-    console.error("データ読み込みエラー:", err);
-    container.innerHTML = "<p>データの読み込みに失敗しました。</p>";
+async function loadData() {
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  data.forEach(entry => {
+    const copy = baseHtml.cloneNode(true);
+    copy.classList.remove('js-base');
+    copy.querySelector('.spreadsheets--name').textContent = entry.name;
+    copy.querySelector('.spreadsheets--type').textContent = entry.type;
+    copy.querySelector('.spreadsheets--source').textContent = entry.source;
+    copy.querySelector('.spreadsheets--img').src = entry.image;
+    spreadsheets.appendChild(copy);
   });
+}
+
+loadData();
