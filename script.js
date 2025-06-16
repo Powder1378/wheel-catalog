fe14d3e31f0fd9d0515e60e4eb86626ae731ca83
powderrrrrr <116692891+Powder1378@users.noreply.github.com>
2025-06-16 10:05:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,38 @@
 const data = [
-  {
-    name: "インフェルノ",
-    image: "./images/SPT_V_1.png"
-  },
-  {
-    name: "ディープファイブ",
-    image: "./images/SPT_V_2.png"
-  },
-  {
-    name: "ロズスピード Mr.V",
-    image: "./images/SPT_V_3.png"
-  },
-  {
-    name: "ダイヤモンドカット",
-    image: "./images/SPT_V_4.png"
-  },
-  {
-    name: "クロノ",
-    image: "./images/SPT_V_5.png"
-  },
-  {
-    name: "フェローチRR",
-    image: "./images/SPT_V_6.png"
-  },
-  {
-    name: "フィフティナイン",
-    image: "./images/SPT_V_7.png"
-  },
-  {
-    name: "マーシー",
-    image: "./images/SPT_V_8.png"
-  },
-  {
-    name: "シンセティックZ",
-    image: "./images/SPT_V_9.png"
-  },
-  {
-    name: "オーガニック・タイプゼロ",
-    image: "./images/SPT_V_10.png"
-  },
-  {
-    name: "エンドーv.1",
-    image: "./images/SPT_V_11.png"
-  },
-  {
-    name: "GTワン",
-    image: "./images/SPT_V_12.png"
-  },
-  {
-    name: "デューバー7",
-    image: "./images/SPT_V_13.png"
-  },
-  {
-    name: "ウザール",
-    image: "./images/SPT_V_14.png"
-  },
-  {
-    name: "グラウンドライド",
-    image: "./images/SPT_V_15.png"
-  },
-  {
-    name: "Sレーサー",
-    image: "./images/SPT_V_16.png"
-  },
-  {
-    name: "ベナム",
-    image: "./images/SPT_V_17.png"
-  },
-  {
-    name: "コスモ",
-    image: "./images/SPT_V_18.png"
-  },
-  {
-    name: "ダッシュVIP",
-    image: "./images/SPT_V_19.png"
-  },
-  {
-    name: "アイスキッド",
-    image: "./images/SPT_V_20.png"
-  },
-  {
-    name: "ラウフェルド",
-    image: "./images/SPT_V_21.png"
-  },
-  {
-    name: "湾岸マスター",
-    image: "./images/SPT_V_22.png"
-  },
-  {
-    name: "スーパーファイブ",
-    image: "./images/SPT_V_23.png"
-  },
-  {
-    name: "エンドーv2",
-    image: "./images/SPT_V_24.png"
-  },
-  {
-    name: "スプリット・シックス",
-    image: "./images/SPT_V_25.png"
-  }
+  { name: "インフェルノ", image: "./images/SPT_V_1.png" },
+  { name: "ディープファイブ", image: "./images/SPT_V_2.png" },
+  { name: "ロズスピード Mr.V", image: "./images/SPT_V_3.png" },
+  { name: "ダイヤモンドカット", image: "./images/SPT_V_4.png" },
+  { name: "クロノ", image: "./images/SPT_V_5.png" },
+  { name: "フェローチRR", image: "./images/SPT_V_6.png" },
+  { name: "フィフティナイン", image: "./images/SPT_V_7.png" },
+  { name: "マーシー", image: "./images/SPT_V_8.png" },
+  { name: "シンセティックZ", image: "./images/SPT_V_9.png" },
+  { name: "オーガニック・タイプゼロ", image: "./images/SPT_V_10.png" },
+  { name: "エンドーv.1", image: "./images/SPT_V_11.png" },
+  { name: "GTワン", image: "./images/SPT_V_12.png" },
+  { name: "デューバー7", image: "./images/SPT_V_13.png" },
+  { name: "ウザール", image: "./images/SPT_V_14.png" },
+  { name: "グラウンドライド", image: "./images/SPT_V_15.png" },
+  { name: "Sレーサー", image: "./images/SPT_V_16.png" },
+  { name: "ベナム", image: "./images/SPT_V_17.png" },
+  { name: "コスモ", image: "./images/SPT_V_18.png" },
+  { name: "ダッシュVIP", image: "./images/SPT_V_19.png" },
+  { name: "アイスキッド", image: "./images/SPT_V_20.png" },
+  { name: "ラウフェルド", image: "./images/SPT_V_21.png" },
+  { name: "湾岸マスター", image: "./images/SPT_V_22.png" },
+  { name: "スーパーファイブ", image: "./images/SPT_V_23.png" },
+  { name: "エンドーv2", image: "./images/SPT_V_24.png" },
+  { name: "スプリット・シックス", image: "./images/SPT_V_25.png" }
 ];
+
+// SPT_M_51 ~ SPT_M_113 を追加
+for (let i = 51; i <= 113; i++) {
+  data.push({
+    name: `SPT_M_${i}`,
+    image: `./images/SPT_M_${i}.png`
+  });
+}
 
 const container = document.getElementById("wheel-container");
 const searchInput = document.getElementById("search");
@@ -109,10 +42,10 @@ function displayData(items) {
   items.forEach(item => {
     const card = document.createElement("div");
     card.className = "wheel-card";
-    card.innerHTML = 
+    card.innerHTML = `
       <img src="${item.image}" alt="${item.name}">
       <h3>${item.name}</h3>
-    ;
+    `;
     container.appendChild(card);
   });
 }
